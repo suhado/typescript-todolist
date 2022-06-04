08271c103e24472044dfd0b26255c0b735b92921
정수하 <suha@jeongsuhaui-MacBookPro.local>
2022-06-04 14:44:02 +0900

    FEAT: TodoList 컴포넌트 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import "./App.css";
 import InputFeild from "./components/InputFeild";
+import TodoList from "./components/TodoList";
 import { Todo } from "./model";
 
 function App() {
@@ -20,6 +21,7 @@ function App() {
     <div className="App">
       <span className="heading">Suha</span>
       <InputFeild todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
+      <TodoList todos={todos} setTodos={setTodos} />
     </div>
   );
 }
