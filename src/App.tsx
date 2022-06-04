2d63a9d643c2853e182c6a1eb19fba62060857e1
정수하 <suha@jeongsuhaui-MacBookPro.local>
2022-06-04 14:16:29 +0900

    FEAT: todos구현 Todo model 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import React, { useState } from "react";
 import "./App.css";
 import InputFeild from "./components/InputFeild";
+import { Todo } from "./model";
 
 function App() {
   const [todo, setTodo] = useState<string>("");
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  console.log(todo);
+
   return (
     <div className="App">
       <span className="heading">Suha</span>
